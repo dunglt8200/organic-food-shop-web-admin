3c1daca7f74b4d9fd83a1974069a4c0555a86dd1
thanhdung <ltdung0802@gmail.com>
2024-09-04 09:13:43 +0700

    fix css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterSetting from './routerSetting';
 import {BrowserRouter} from 'react-router-dom';
-import "./css/main.css";
+import "./static/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
