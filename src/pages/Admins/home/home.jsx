8517c4bdc13e5d7339e6a84faf0b4b9c9f4dd7db
thanhdung <ltdung0802@gmail.com>
2024-09-11 16:12:47 +0700

    add chart

--- src/pages/Admins/home/home.jsx
+++ src/pages/Admins/home/home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import HomeBanner from "../../../static/img/farm-banner.jpg";
 import "./home.style.css";
+import ChartStatistics from "../../common/chart";
+import { Pie, Bar, Line } from 'react-chartjs-2';
 
 function Home() {
     return (
         <div className="div-home">
-            <img src={HomeBanner} alt="" />
+            <div className="div-chart">
+                <ChartStatistics type_chart={Bar}/>
+                <p>Thống kê loại sản phẩm</p>
+            </div> 
+                 
+            <div className="div-chart">
+                <ChartStatistics type_chart={Line}/>
+                <p>Thống kê sản phẩm</p>
+            </div>                          
         </div>
     ) 
 }
---
+++ src/pages/common/chart.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, LineElement, PointElement } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, LineElement, PointElement);
+
+const ChartStatistics = ({ type_chart: ChartComponent }) => {
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <ChartComponent data={data} />;
+};
+
+export default ChartStatistics;
