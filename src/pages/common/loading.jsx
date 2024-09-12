52c82704db01a26bf5bbf721570a4555c97e025a
thanhdung <ltdung0802@gmail.com>
2024-09-12 14:09:20 +0700

    update loading

--- src/pages/common/loading.jsx
+++ src/pages/common/loading.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HashLoader from 'react-spinners/HashLoader';
+import ScaleLoader from 'react-spinners/ScaleLoader';
 
 const Loading = ({ loading }) => {
     return (
         <>
             {loading && (
                 <div className="loading-container">
-                    <HashLoader color="#339933" loading={loading} size={50} />
+                    <ScaleLoader color="#cce0e6" loading={loading} size={50} />
                 </div>
             )}
         </>
